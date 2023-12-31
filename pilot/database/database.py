7bee62675deef9bab1cec95c9f6fae8e2219d0f5
Sourcery AI <>
2023-12-31 03:12:29 +0000

    'Refactored by Sourcery'

--- pilot/database/database.py
+++ pilot/database/database.py
@@ -56,7 +56,11 @@ def get_created_apps_with_steps():
     apps = get_created_apps()
     for app in apps:
         app['id'] = str(app['id'])
-        app['steps'] = [step for step in STEPS[:STEPS.index(app['status']) + 1]] if app['status'] is not None else []
+        app['steps'] = (
+            list(STEPS[: STEPS.index(app['status']) + 1])
+            if app['status'] is not None
+            else []
+        )
         app['development_steps'] = get_all_app_development_steps(app['id'])
         # TODO this is a quick way to remove the unnecessary fields from the response
         app['development_steps'] = [{k: v for k, v in dev_step.items() if k in {'id', 'created_at'}} for dev_step in
@@ -70,12 +74,10 @@ def get_all_app_development_steps(app_id):
 
 def save_user(user_id, email, password):
     try:
-        user = User.get(User.id == user_id)
-        return user
+        return User.get(User.id == user_id)
     except DoesNotExist:
         try:
-            existing_user = User.get(User.email == email)
-            return existing_user
+            return User.get(User.email == email)
         except DoesNotExist:
             return User.create(id=user_id, email=email, password=password)
 
@@ -101,8 +103,7 @@ def get_user(user_id=None, email=None):
         query.append(User.email == email)
 
     try:
-        user = User.get(reduce(operator.or_, query))
-        return user
+        return User.get(reduce(operator.or_, query))
     except DoesNotExist:
         raise ValueError("No user found with provided id or email")
 
@@ -189,8 +190,7 @@ def save_progress(app_id, step, data):
 
 def get_app(app_id, error_if_not_found=True):
     try:
-        app = App.get(App.id == app_id)
-        return app
+        return App.get(App.id == app_id)
     except DoesNotExist:
         if error_if_not_found:
             raise ValueError(f"No app with id: {app_id}")
@@ -437,8 +437,12 @@ def save_file_description(project, path, name, description):
 def save_feature(app_id, summary, messages, previous_step):
     try:
         app = get_app(app_id)
-        feature = Feature.create(app=app, summary=summary, messages=messages, previous_step=previous_step)
-        return feature
+        return Feature.create(
+            app=app,
+            summary=summary,
+            messages=messages,
+            previous_step=previous_step,
+        )
     except DoesNotExist:
         raise ValueError(f"No app with id: {app_id}")
 
@@ -503,8 +507,6 @@ def create_database():
 
         cursor.close()
         conn.close()
-    else:
-        pass
 
 
 def tables_exist():
@@ -514,8 +516,6 @@ def tables_exist():
                 database.get_tables().index(table._meta.table_name)
             except ValueError:
                 return False
-    else:
-        pass
     return True
 
 
